b86e889636263c52e264b33d2a74446328cb6418
Thony <thon0101@stud.kea.dk>
2022-08-29 12:32:02 +0200

    noter til app.js indsat

--- 1._basic_Express/app.js
+++ 1._basic_Express/app.js
@@ -1,8 +1,15 @@
+//npm install skal installeres ved ny mappe (node_modules vil stå til venstre)
+//require gør at vi kan benytte express
 const express = require("express"); 
+//Her laves en variabel hvor vi gemmer metoden ekspress i da vi gerne vil
+//benytte den i vores endpoint
 const app = express();
-    //Endpoint
+//Endpoint
+//Her benytter vi app med en GET for at få response på message vi gerne vil have 
+//Eksikveret
 app.get("/", (request, response) => {
     response.send({ message: "Created my first route. Check."});
 });
 //Set port 
+//Her sætter vi på hvilken port vi gerne vil have skrevet ud på fx. localhost8080
 app.listen(8080); 
